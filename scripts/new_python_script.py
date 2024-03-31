7aefd7851730e1442f9fce7f3b8c00fab707c4a6
Vladislav Pareiko <parieiko.vlad@gmail.com>
2024-03-31 23:07:35 +0400

    Update greeting. Write greet function

--- scripts/new_python_script.py
+++ scripts/new_python_script.py
@@ -1,1 +1,12 @@
-print("JUST NEW GREETING")
+us_name: str = input("Provide your name: ")
+us_surname: str = input("Provide your surname: ")
+us_age: int = int(input("Provide your age: "))
+
+
+def user_greeting(name: str, surname: str, age: int) -> str:
+	greet: str = f"My name is {name} {surname}. I'm {age} years old."
+
+
+greet = user_greeting(us_name, us_surname, us_age)
+
+print(greet)

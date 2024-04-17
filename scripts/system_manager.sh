031eaf0a8d4cdd891b66b5d74b54cb79e66580c9
Openiuk <openyusha@gmail.com>
2024-04-17 13:40:50 +0200

    Create system manager script

---
+++ scripts/system_manager.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Please provide your name!"
+read USER_NAME
+if [[ $USER_NAME == "" ]]
+then 
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')] ERROR You should enter your name! " >> ./eroors.log
+else
+
+echo "Greeting, $USER_NAME"
+date "+%d-%m-%Y"
+date "+%H:%M:%S"
+ps aux | grep -v "ps aux" | grep -v "grep" | wc -l
+cat /etc/os-release
+fi
